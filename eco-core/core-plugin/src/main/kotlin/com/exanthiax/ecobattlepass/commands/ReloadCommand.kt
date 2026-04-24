115536ce8aa216fae61adebc874521f9958bfc29
NicoNeko <nicuch100@gmail.com>
2026-04-24 22:45:39 +0300

    Add folia support (#13)
    Co-authored-by: Callum Johnson <74352969+CallumJohnson@users.noreply.github.com>

--- eco-core/core-plugin/src/main/kotlin/com/exanthiax/ecobattlepass/commands/ReloadCommand.kt
+++ eco-core/core-plugin/src/main/kotlin/com/exanthiax/ecobattlepass/commands/ReloadCommand.kt
@@ -1,28 +1,31 @@
 package com.exanthiax.ecobattlepass.commands
 
 import com.exanthiax.ecobattlepass.battlepass.BattlePasses
-import com.exanthiax.ecobattlepass.categories.Categories
 import com.exanthiax.ecobattlepass.plugin
-import com.exanthiax.ecobattlepass.quests.BattleQuests
-import com.exanthiax.ecobattlepass.rewards.Rewards
-import com.exanthiax.ecobattlepass.tasks.BattleTasks
+import com.willfp.eco.core.Prerequisite
 import com.willfp.eco.core.command.impl.PluginCommand
-import com.willfp.eco.util.NumberUtils
 import com.willfp.eco.util.StringUtils
 import com.willfp.eco.util.toNiceString
 import org.bukkit.command.CommandSender
 
-object ReloadCommand: PluginCommand(
+object ReloadCommand : PluginCommand(
     plugin,
     "reload",
     "ecobattlepass.command.reload",
     false
 ) {
     override fun onExecute(sender: CommandSender, args: List<String>) {
-        sender.sendMessage(
-            plugin.langYml.getMessage("reloaded", StringUtils.FormatOption.WITHOUT_PLACEHOLDERS)
-                .replace("%time%", plugin.reloadWithTime().toNiceString())
-                .replace("%count%", BattlePasses.values().size.toString())
-        )
+        val runnable = Runnable {
+            sender.sendMessage(
+                plugin.langYml.getMessage("reloaded", StringUtils.FormatOption.WITHOUT_PLACEHOLDERS)
+                    .replace("%time%", plugin.reloadWithTime().toNiceString())
+                    .replace("%count%", BattlePasses.values().size.toString())
+            )
+        }
+        if (Prerequisite.HAS_FOLIA.isMet) {
+            plugin.scheduler.runTask(runnable)
+        } else {
+            runnable.run()
+        }
     }
 }
